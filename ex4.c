39ec7115ead710a381f6717736eeb57016569999
Blairi <axel.avi.fer@gmail.com>
2024-04-25 13:30:20 -0600

    ex4

---
+++ ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    
+    fork();
+    fork();
+    fork();
+    printf("\tSe ha creado un proceso (%d) y mi padre es (%d)\n", getpid(), getppid());
+
+    return 0;
+}
